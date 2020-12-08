9ddcbfe188af005c3ee62fd24c4d2158db453402
breney <brunopy2@gmail.com>
2020-12-08 18:47:52 +0000

    some front end

---
+++ database/migrations/2014_10_12_000000_create_book-authors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book-authors', function (Blueprint $table) {
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('authors');
+            $table->foreign('book_id')
+                ->references('id')
+                ->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
